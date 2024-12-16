d873a5776b409f41747f235618ead7c9c707d367
jonny <johannes.reinwald@tutanota.com>
2024-12-16 16:45:10 +0100

    making name input work

--- test.py
+++ test.py
@@ -1,4 +1,15 @@
-name = input("Enter your name: ")
+def draw_christmas_tree(height):
+    for i in range(height):
+        spaces = ' ' * (height - i - 1)
+        stars = '*' * (2 * i + 1)
+        print(spaces + stars)
 
-print(f"hello, {name}")
 
+    trunk_width = height // 3 if height > 5 else 1
+    trunk_height = 2
+    trunk_spaces = ' ' * (height - trunk_width // 2 - 1)
+
+    for _ in range(trunk_height):
+        print(trunk_spaces + '|' * trunk_width)
+
+draw_christmas_tree(10)
